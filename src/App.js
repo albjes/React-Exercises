b34ded63b90cd79ff65f7f0e75236c38d5ae7fd2
Jesus <jesus.fuentes@gmail.com>
2022-08-15 13:38:18 +0200

    Prueba cambio de nombre git

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div>
       <Nav />
       <h2 className="text-3xl w-full text-center bg-gray-200 text-gray-700 p-8">
-        Ejercicios con React
+        Ejercicio con React
       </h2>
       <CrudApp />
     </div>
